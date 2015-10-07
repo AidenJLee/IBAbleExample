e0fbbdb22f0484594fb342522366170327f96706
AidenJLee <entist@me.com>
2015-10-07 13:55:04 +0900

    Initial Commit

---
+++ IBAbleExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IBAbleExample
+//
+//  Created by aidenjlee on 2015. 10. 7..
+//  Copyright © 2015년 entist. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IBAbleExample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  IBAbleExample
+//
+//  Created by aidenjlee on 2015. 10. 7..
+//  Copyright © 2015년 entist. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
